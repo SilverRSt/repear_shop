ec104c4ddecd13bb49c609c72b176f3a39e7f5a6
SilverRSt <ralist@mail.bg>
2022-10-25 18:28:58 +0300

    Initial commit

---
+++ repear_shop/src/main/java/com/example/repear_shop/RepairShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.repear_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairShopApplication.class, args);
+	}
+
+}
---
+++ repear_shop/src/test/java/com/example/repear_shop/RepearShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repear_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepearShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
